63e8c1fd4c8fae490831723d8b5d42d1fdee659c
mike-f50 <58040070+mike-f50@users.noreply.github.com>
2020-04-03 00:00:54 +0100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry : "./getValidation/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}
