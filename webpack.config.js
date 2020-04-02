60d02d3811fb8db2fa1d8fd9e4107a5bb73f7700
mike-f50 <58040070+mike-f50@users.noreply.github.com>
2020-04-03 00:08:23 +0100

    Update webpack.config.js
    Removing babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: {}
       }
     ]
   }
