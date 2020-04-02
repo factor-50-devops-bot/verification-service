22bfc3261e217be30605c5285095c1645a8b2eb2
mike-f50 <58040070+mike-f50@users.noreply.github.com>
2020-04-03 00:27:43 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  target: 'node'
+  target: 'node',
+  entry : "./getValidation/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
 }
