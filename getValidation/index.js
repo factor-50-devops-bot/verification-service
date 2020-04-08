62ae511f4714dbd37f90c513f2bd32341d86c74d
Geoff Burnhill <geoff@factor-50.co.uk>
2020-04-08 17:01:57 +0100

    Moved Security Key To KeyVault

--- getValidation/index.js
+++ getValidation/index.js
@@ -7,7 +7,7 @@ const fetch = require("node-fetch");
 // Yoti connection //
 const CLIENT_SDK_ID = process.env["yoti_api_key"];
 const PEM_PATH = __dirname + "//resources//security.pem";
-const PEM_KEY = fs.readFileSync(PEM_PATH);
+const PEM_KEY = process.env.yoti-pem;
 const yotiClient = new yoti.Client(CLIENT_SDK_ID, PEM_KEY);
 
 // Database connection //
