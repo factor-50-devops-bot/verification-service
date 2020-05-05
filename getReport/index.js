b333f0adbe9c4a3710b97ddfd58d04f118e81eef
Geoff Burnhill <geoff@factor-50.co.uk>
2020-05-05 11:10:41 +0100

    Unnecessary Extensibility Addition
    Recoded to allow for reporting of any DB query

--- getReport/index.js
+++ getReport/index.js
@@ -17,49 +17,62 @@ module.exports = function(context, req) {
   twoHoursAgo.setHours(twoHoursAgo.getHours() - 2);
 
   var timePoints = [
-    { name: "2hrs", string: `c.datestamp >= "${twoHoursAgo}"` },
-    { name: "AllTime", string: `true` },
-    { name: "Today", string: `c.datestamp >= "${sinceMidnight}"` }
+    { name: "last2Hours", string: `c.datestamp >= "${twoHoursAgo}"` },
+    { name: "today", string: `c.datestamp >= "${sinceMidnight}"` },
+    { name: "sinceLaunch", string: `true` },
+    
   ];
   var variables = [
     { name: "total", string: "true" },
     { name: "verified", string: "c.verified = true" },
-    { name: "unverified", string: "c.verified = false" }
+    { name: "unverified", string: "c.verified = false" },
+    { name: "error", string: "c.isError = true" }
   ];
-  var queries = [];
 
-  timePoints.map(timePoint => {
-    variables.map(variable => {
-      queries.push({
-        name: `${variable.name}${timePoint.name}`,
-        string: `${countString} ${variable.string} AND ${timePoint.string}`
-      });
-    });
+  var metrics = variables.map(variable => {
+    return {
+      section: variable.name,
+      queries: timePoints.map(timePoint => {
+        return {
+          name: `${timePoint.name}`,
+          string: `${countString} ${variable.string} AND ${timePoint.string}`
+        };
+      })
+    };
   });
 
-  var results = [];
-
-  // Then run container.items.query to extract promises and resolve promises as array of {query.name, datum.resources[0]}
-  queries.map(query => {
-    results.push(
-      new Promise((resolve, reject) => {
-        container.items
-          .query(query.string)
-          .fetchAll()
-          .then(result =>
-            resolve({ name: query.name, result: result.resources[0] })
-          )
-          .catch(err => reject(err));
-      })
-    );
+  var results = metrics.map(metric => {
+    return new Promise((resolve, reject) => {
+      Promise.all(
+        metric.queries.map(query => {
+          return new Promise((resolve, reject) => {
+            container.items
+              .query(query.string)
+              .fetchAll()
+              .then(queryResult =>
+                resolve({ name: query.name, result: queryResult.resources[0] })
+              )
+              .catch(err => reject(err));
+          });
+        })
+      )
+        .then(sectionResult =>
+          resolve({ section: metric.section, results: sectionResult })
+        )
+        .catch(err => reject(err));
+    });
   });
 
   Promise.all(results)
     .then(data => {
+    var reportLines = data.map(datum => {var reportObject = {section: datum.section};
+    datum.results.forEach(result => reportObject[result.name] = result.result);
+    return reportObject;}
+    )
       context.res = {
         status: 200,
-        body: data,
-        headers: {
+        body: reportLines,
+        headers: { 
           "Content-Type": "application/json"
         }
       };
